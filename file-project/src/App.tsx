dd63a65715558d0560938b18609776432ad14c39
Marina Gromova <krapiva87sstu@mail.ru>
2025-04-07 11:01:03 +0300

    рефакторинг

--- file-project/src/App.tsx
+++ file-project/src/App.tsx
@@ -1,29 +1,17 @@
-import "./App.module.scss";
-import { useBackGroundResize } from "./components/Project/hooks/useBackGroundResize";
-import { Layout } from "./components/Project/layout/Layout";
+import './App.module.scss'
+import { useBackGroundResize } from './components/Project/hooks/useBackGroundResize'
+import { Layout } from './components/Project/layout/Layout'
 
 function App() {
-  const colorObject = useBackGroundResize();
+	const colorObject = useBackGroundResize()
 
-  return (
-    <>
-      <div style={colorObject}>
-        <Layout />
-      </div>
-    </>
-  );
-
-  // <>
-  {
-    /* //TODO */
-  }
-  {
-    /* <h4>
-        Меняем иконку при ховере кнопки на OutLinedButton, без ховера Button
-      </h4> */
-  }
-
-  // </>
+	return (
+		<>
+			<div style={colorObject}>
+				<Layout />
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
