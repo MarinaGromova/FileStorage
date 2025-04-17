16197827281a407d13e85487f43663ca474382cc
Marina Gromova <krapiva87sstu@mail.ru>
2025-04-17 14:13:18 +0300

    цвет кнопки

--- file-project/src/components/layout/header/Header.tsx
+++ file-project/src/components/layout/header/Header.tsx
@@ -1,14 +1,26 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../../../App'
 import styles from '../Layout.module.scss'
 import { Menu } from './../hamburger/menu'
 
 export const Header = () => {
+	const { theme, switchTheme } = useContext(ThemeContext)
+
 	return (
 		<>
 			<button className={styles.button}>
 				<Link to={'profile'}>
 					<img src='/header/profile.svg' alt='icon minion' />
 				</Link>
+			</button>
+			<button
+				className={styles.checkbox}
+				type='button'
+				style={theme}
+				onClick={switchTheme}
+			>
+				Цвет кнопки
 			</button>
 			<Menu />
 		</>
